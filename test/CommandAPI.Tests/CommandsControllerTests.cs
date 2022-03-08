d7594e1c8c7835e979b6781686ae4e4660192bc4
Ritesh <ritesh.patel121@gmail.com>
2022-03-08 09:16:03 +0000

    fixed broken unit test

--- test/CommandAPI.Tests/CommandsControllerTests.cs
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -317,7 +317,7 @@ namespace CommandAPI.Tests
             var result = controller.DeleteCommand(0);
 
             //Assert
-            Assert.IsType<OkResult>(result);
+            Assert.IsType<NotFoundResult>(result);
             
         }
 
